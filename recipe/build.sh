9cd12a4b648d425a135a3e7c728fe410896fd603
Steven <scrass@anaconda.com>
2025-07-08 14:09:54 -0600

    add one more Bazel flag to disable specific undeclared inclusion checking

--- recipe/build.sh
+++ recipe/build.sh
@@ -104,7 +104,9 @@ build --define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include
 build --local_cpu_resources=${CPU_COUNT}
 build --features=-strict_header_checking
 build --features=-layering_check
+build --features=-parse_headers_verifies_modules
 build --experimental_strict_action_env=false
+build --nocheck_visibility
 EOF
 
 # Never use the Apple toolchain - critical fix for macOS ARM64
