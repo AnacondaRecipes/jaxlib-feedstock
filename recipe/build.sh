ad50f31dc2bcc35d556fcec6856e17a63cb2adf3
Steven <scrass@anaconda.com>
2025-07-08 14:00:57 -0600

    remove bad flag

--- recipe/build.sh
+++ recipe/build.sh
@@ -102,7 +102,6 @@ build --toolchain_resolution_debug
 build --define=PREFIX=${PREFIX}
 build --define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include
 build --local_cpu_resources=${CPU_COUNT}
-build --copt=-isystem${BUILD_PREFIX}/lib/clang/17/include
 build --features=-strict_header_checking
 EOF
 
