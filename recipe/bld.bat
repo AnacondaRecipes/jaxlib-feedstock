c9c7270de0fbc7e139a7455ce1cac7c4bf17dbaf
Onur Bingol <obingol@anaconda.com>
2025-11-11 16:10:22 -0800

    generate whl with correct name and copy

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,7 +9,13 @@
 @echo on
 setlocal EnableDelayedExpansion
 
-set JAX_RELEASE=%PKG_VERSION%
+:: if JAX_RELEASE or JAXLIB_RELEASE are set: version looks like "0.7.2"
+:: if JAX_NIGHTLY or JAXLIB_NIGHTLY are set: version looks like "0.7.2.devX"
+:: if none are set: version looks like "0.7.2.devX+Y" or "0.7.2.dev0+selfbuilt"
+:: where X is the build number, Y is the git hash
+:: This is required to get '%SP_DIR~\jaxlib-{{ version }}.dist-info' directory name correctly
+set JAXLIB_RELEASE=1
+set ML_WHEEL_TYPE=release
 
 :: Necessary variables to make conda-build working
 set BAZEL_VS="%VSINSTALLDIR%"
@@ -53,7 +59,16 @@ echo build --action_env=CC=%CLANG_CL_PATH% >> .bazelrc.user
 echo build --action_env=CXX=%CLANG_CL_PATH% >> .bazelrc.user
 echo build --repo_env=CC=%CLANG_CL_PATH% >> .bazelrc.user
 echo build --repo_env=CXX=%CLANG_CL_PATH% >> .bazelrc.user
-echo build --repo_env=BAZEL_USE_CPP_ONLY_TOOLCHAIN=1 >> .bazelrc.user
+echo build --repo_env=BAZEL_USE_CPP_ONLY_TOOLCHAIN=1 >> .bazelrc.userls
+
+:: Set wheel version string (seems to be required alongside JAXLIB_RELEASE env variable)
+:: * `0.7.2.dev0+selfbuilt` (local build, default build rule behavior): `--repo_env=ML_WHEEL_TYPE=snapshot`
+:: * `0.7.2` (release): `--repo_env=ML_WHEEL_TYPE=release`
+:: * `0.7.2rc1` (release candidate): `--repo_env=ML_WHEEL_TYPE=release --repo_env=ML_WHEEL_VERSION_SUFFIX=rc1`
+:: * `0.7.2.dev20250128+3e75e20c7` (nightly build): `--repo_env=ML_WHEEL_TYPE=custom --repo_env=ML_WHEEL_BUILD_DATE=20250128 --repo_env=ML_WHEEL_GIT_HASH=$(git rev-parse HEAD)`
+:: Ref: https://github.com/jax-ml/jax/commit/d424f5b5b38b75b6577d2c30532abbb693353742
+echo build --repo_env=ML_WHEEL_TYPE=release >> .bazelrc.user
+echo build --repo_env=JAXLIB_RELEASE=%JAXLIB_RELEASE% >> .bazelrc.user
 
 :: _m_prefetchw is declared in both Clang and Windows SDK
 :: This can be removed with Clang 21
@@ -77,6 +92,12 @@ echo build --copt=-DNOGDI >> .bazelrc.user
   --bazel_options="--experimental_ui_max_stdouterr_bytes=8000000"
 if %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
 
+:: Manually copy wheel with correct pattern (build.py is using wrong pattern)
+if not exist dist mkdir dist
+copy /Y bazel-bin\jaxlib\tools\dist\jaxlib-*.whl dist\
+if %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
+
+:: Clean up
 bazel clean --expunge
 bazel shutdown
 
