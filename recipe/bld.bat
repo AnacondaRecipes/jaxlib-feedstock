eee1f23fdd29fc7da33392fc222c8a53b858b692
Brian Wing <bwing@anaconda.com>
2025-12-17 17:20:49 -0500

    jaxlib v0.7.2 (#20)
    jaxlib 0.7.2
    
    **Destination channel:** defaults
    
    ### Links
    
    - [PKG-8729](https://anaconda.atlassian.net/browse/PKG-8729)
    - [Upstream repository]()
    - [Upstream changelog/diff]()
    - Relevant dependency PRs:
      - AnacondaRecipes/bazel-feedstock#23
      - AnacondaRecipes/ctng-compilers-feedstock#2
      - AnacondaRecipes/ctng-compiler-activation-feedstock#6
      - AnacondaRecipes/bazel-toolchain-feedstock/pull/9
    
    ### Explanation of changes:
    
    - ~PBP linux-64 runners are running out of time to build~ -- this is
    solved
    - ~PBP linux-aarch64 runners are running out of memory~ -- this is
    solved
    - ~Build uses flags only found in GCC 13 and later~ -- using `clang`
    instead of `gcc`
      - ~Bumped build to use GCC 14.3.0~
    - ~CUDA 12.4 needs to be rebuilt with GCC 14.3.0 to allow CUDA builds on
    Linux again~ -- it might be easier to be handled on a separate ticket
    - ~Almost got Windows builds working, they _should_ be able to build the
    CUDA stuff too since they won't run into GCC version issues~ -- `jaxlib`
    won't support CUDA on Windows. It might if we could use a different
    build toolchain, but not with `clang-cl`.
    - Added `bld.bat` for Windows builds. Windows builds are using
    `clang-cl`.
    - Populate custom Bazel configuration generated by feedstock's build
    scripts to `.bazelrc.user` file. Please refer to the [end of the
    .bazelrc](https://github.com/jax-ml/jax/blob/jax-v0.7.2/.bazelrc#L526)
    configuration file.
    - XLA unvendoring patches are only for Linux and macOS. Windows builds
    are kept vendored.
    - XLA is patched to use Abseil v20250814.1 as a dependency on Linux and
    macOS
    
    ---------
    
    Co-authored-by: Onur Bingol <obingol@anaconda.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,19 +1,87 @@
-set BAZEL_VS="%VSINSTALLDIR%"
-set BAZEL_VC="%VSINSTALLDIR%/VC"
-set CLANG_COMPILER_PATH=%BUILD_PREFIX:\=/%/Library/bin/clang.exe
-set BAZEL_LLVM=%BUILD_PREFIX:\=/%/Library/
+:: Note: TF_SYSTEM_LIBS variable won't work on Windows
+:: Ref: https://github.com/tensorflow/tensorflow/blob/master/.bazelrc#L476
+:: Therefore, for any of the packages that rely on xla (tensorflow and jaxlib),
+:: we've made the decision to just use the vendored dependencies on Windows.
 
-@REM   - if JAX_RELEASE or JAXLIB_RELEASE are set: version looks like "0.4.16"
-@REM   - if JAX_NIGHTLY or JAXLIB_NIGHTLY are set: version looks like "0.4.16.dev20230906"
-@REM   - if none are set: version looks like "0.4.16.dev20230906+ge58560fdc
+@echo on
+setlocal EnableDelayedExpansion
+
+:: if JAX_RELEASE or JAXLIB_RELEASE are set: version looks like "0.7.2"
+:: if JAX_NIGHTLY or JAXLIB_NIGHTLY are set: version looks like "0.7.2.devX"
+:: if none are set: version looks like "0.7.2.devX+Y" or "0.7.2.dev0+selfbuilt"
+:: where X is the build number, Y is the git hash
+:: This is required to get '%SP_DIR~\jaxlib-{{ version }}.dist-info' directory name correctly
 set JAXLIB_RELEASE=1
 
-@REM Note: TF_SYSTEM_LIBS don't work on windows per https://github.com/openxla/xla/blob/edf18ce242f234fbd20d1fbf4e9c96dfa5be2847/.bazelrc#L383
+:: Set wheel version string (seems to be required alongside JAXLIB_RELEASE env variable)
+:: * `0.7.2.dev0+selfbuilt` (local build, default build rule behavior): `--repo_env=ML_WHEEL_TYPE=snapshot`
+:: * `0.7.2` (release): `--repo_env=ML_WHEEL_TYPE=release`
+:: * `0.7.2rc1` (release candidate): `--repo_env=ML_WHEEL_TYPE=release --repo_env=ML_WHEEL_VERSION_SUFFIX=rc1`
+:: * `0.7.2.dev20250128+3e75e20c7` (nightly build): `--repo_env=ML_WHEEL_TYPE=custom --repo_env=ML_WHEEL_BUILD_DATE=20250128 --repo_env=ML_WHEEL_GIT_HASH=$(git rev-parse HEAD)`
+:: Ref: https://github.com/jax-ml/jax/commit/d424f5b5b38b75b6577d2c30532abbb693353742
+set ML_WHEEL_TYPE=release
 
-%PYTHON% build/build.py --bazel_options=--config=win_clang --verbose --use_clang=true --clang_path=%CLANG_COMPILER_PATH%
+:: Necessary variables to make conda-build working
+set BAZEL_VS="%VSINSTALLDIR%"
+set BAZEL_VC="%VSINSTALLDIR%/VC"
+set BAZEL_LLVM=%BUILD_PREFIX:\=/%/Library/
+set CLANG_COMPILER_PATH=%BUILD_PREFIX:\=/%/Library/bin/clang.exe
+
+:: Converted from build.sh
+echo build --logging=6 >> .bazelrc.user
+echo build --verbose_failures >> .bazelrc.user
+echo build --toolchain_resolution_debug >> .bazelrc.user
+echo build --define=PREFIX=%PREFIX:\=/% >> .bazelrc.user
+echo build --define=PROTOBUF_INCLUDE_PATH=%PREFIX:\=/%/include >> .bazelrc.user
+echo build --local_resources=cpu=%CPU_COUNT% >> .bazelrc.user
+echo build --repo_env=GRPC_BAZEL_DIR=%PREFIX:\=/%/share/bazel/grpc/bazel >> .bazelrc.user
+
+:: _m_prefetchw is declared in both Clang and Windows SDK
+:: This can be removed after Clang 21 is released
+echo build --copt=-D__PRFCHWINTRIN_H >> .bazelrc.user
+
+:: Build and install jaxlib
+:: --wheels=jaxlib
+::      to explicitly build jaxlib wheels only
+:: --use_clang=true
+::to use the clang as recommended by the upstream
+:: --clang_path=%CLANG_COMPILER_PATH%
+::      to use the clang from the conda-build environment
+:: --python_version=%PY_VER% is
+::      not directly required and 99.99% of the time hermetic builds use the conda-build environment python version,
+::      but we just want to make sure it is always the same as conda-build environment python version.
+::      Therefore, this is here for just a bit of extra caution.
+:: --bazel_options=--config=win_clang
+::      to use the win_clang config defined in JAX .bazelrc file
+::      'win_clang' config allows the build system to use 'clang-cl.exe' instead of 'cl.exe'.
+:: --bazel_options="--experimental_ui_max_stdouterr_bytes=8000000"
+::      to increase the size of the buffer for stderr output
+::      If this number is too small, the errors will be truncated.
+%PYTHON% build/build.py build ^
+    --verbose ^
+    --wheels=jaxlib ^
+    --use_clang=true ^
+    --clang_path=%CLANG_COMPILER_PATH% ^
+    --python_version=%PY_VER% ^
+    --bazel_options=--config=win_clang ^
+    --bazel_options="--experimental_ui_max_stdouterr_bytes=8000000"
+if %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
+
+if not exist dist mkdir dist
+copy /Y bazel-bin\jaxlib\tools\dist\jaxlib-*.whl dist\
+if %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
+
+:: Clean up Bazel artifacts
 bazel clean --expunge
 bazel shutdown
 
-%PYTHON% -m pip install --find-links=dist jaxlib --no-build-isolation --no-deps
+:: Install generated wheels
+%PYTHON% -m pip install --find-links=dist jaxlib -vv --no-build-isolation --no-deps
+if %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
 
-copy %PREFIX%\Lib\site-packages\jaxlib\mlir\_mlir_libs\*.dll %LIBRARY_BIN%\
+:: Copy DLL files to the conda bin dir
+:: Necessary to eliminate overlinking errors when --error-overlinking is passed to conda-build
+for /r "%PREFIX%\Lib\site-packages\jaxlib" %%i in (*.dll) do (
+    echo Copying %%~nxi
+    copy /Y "%%i" "%LIBRARY_BIN%\"
+)
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,132 +1,263 @@
 #!/bin/bash
-
 set -euxo pipefail
+
+export JAX_RELEASE=1
+
+# Workaround a timestamp issue in rattler-build
+# https://github.com/prefix-dev/rattler-build/issues/1865
+touch -m -t 203510100101 $(find $BUILD_PREFIX/share/bazel/install -type f)
+
+$RECIPE_DIR/add_py_toolchain.sh
 
 if [[ "${target_platform}" == osx-* ]]; then
   export LDFLAGS="${LDFLAGS} -lz -framework CoreFoundation -Xlinker -undefined -Xlinker dynamic_lookup"
+  # Remove stdlib=libc++; this is the default and errors on C sources.
+  export CXXFLAGS="${CXXFLAGS/-stdlib=libc++} -D_LIBCPP_DISABLE_AVAILABILITY"
 else
   export LDFLAGS="${LDFLAGS} -lrt"
+
+  # See https://github.com/llvm/llvm-project/issues/85656
+  # Otherwise, this will cause linkage errors with a GCC-built abseil
+  export CXXFLAGS="${CXXFLAGS} -fclang-abi-compat=17"
 fi
-export CFLAGS="${CFLAGS} -DNDEBUG"
-export CXXFLAGS="${CXXFLAGS} -DNDEBUG"
-
-export BUILD_FLAGS="--target_cpu_features default --enable_mkl_dnn"
-
-#  - if JAX_RELEASE or JAXLIB_RELEASE are set: version looks like "0.4.16"
-#  - if JAX_NIGHTLY or JAXLIB_NIGHTLY are set: version looks like "0.4.16.dev20230906"
-#  - if none are set: version looks like "0.4.16.dev20230906+ge58560fdc
-export JAXLIB_RELEASE=1
-
-if [[ ${cuda_compiler_version} != "None" ]]; then
-  export HERMETIC_CUDA_COMPUTE_CAPABILITIES=sm_60,sm_70,sm_75,sm_80,sm_86,sm_89,sm_90,compute_90
-  export CUDA_HOME="${BUILD_PREFIX}/targets/x86_64-linux"
-  export PATH=$PATH:${BUILD_PREFIX}/nvvm/bin
- 
-  # XLA can only cope with a single cuda header include directory, merge both
-  rsync -a ${PREFIX}/targets/x86_64-linux/include/ ${BUILD_PREFIX}/targets/x86_64-linux/include/
-
-  # Although XLA supports a non-hermetic build, it still tries to find headers in the hermetic locations.
-  # We do this in the BUILD_PREFIX to not have any impact on the resulting jaxlib package.
-  # Otherwise, these copied files would be included in the package.
-  rm -rf ${BUILD_PREFIX}/targets/x86_64-linux/include/third_party
-  mkdir -p ${BUILD_PREFIX}/targets/x86_64-linux/include/third_party/gpus/cuda/extras/CUPTI
-  cp -r ${PREFIX}/targets/x86_64-linux/include ${BUILD_PREFIX}/targets/x86_64-linux/include/third_party/gpus/cuda/
-  cp -r ${PREFIX}/targets/x86_64-linux/include ${BUILD_PREFIX}/targets/x86_64-linux/include/third_party/gpus/cuda/extras/CUPTI/
-  mkdir -p ${BUILD_PREFIX}/targets/x86_64-linux/include/third_party/gpus/cudnn
-  cp ${PREFIX}/include/cudnn.h ${BUILD_PREFIX}/targets/x86_64-linux/include/third_party/gpus/cudnn/
-
-  export LOCAL_CUDA_PATH="${BUILD_PREFIX}/targets/x86_64-linux"
-  export LOCAL_CUDNN_PATH="${PREFIX}/targets/x86_64-linux"
-  export LOCAL_NCCL_PATH="${PREFIX}/targets/x86_64-linux"
-  export TF_CUDA_VERSION="${cuda_compiler_version}"
-  export TF_CUDNN_VERSION="${cudnn}"
-  export TF_NEED_CUDA=1
-  export TF_NCCL_VERSION=$(pkg-config nccl --modversion | grep -Po '\d+\.\d+')
-
-  mkdir -p ${BUILD_PREFIX}/targets/x86_64-linux/bin
-  ln -s $(which ptxas) ${BUILD_PREFIX}/targets/x86_64-linux/bin/ptxas
-  ln -s $(which nvlink) ${BUILD_PREFIX}/targets/x86_64-linux/bin/nvlink
-  ln -s $(which fatbinary) ${BUILD_PREFIX}/targets/x86_64-linux/bin/fatbinary
-
-  export BUILD_FLAGS="${BUILD_FLAGS} \
-                      --enable_cuda \
-                      --enable_nccl \
-                      --cuda_compute_capabilities=$HERMETIC_CUDA_COMPUTE_CAPABILITIES \
-                      --cuda_version=$TF_CUDA_VERSION \
-                      --cudnn_version=$TF_CUDNN_VERSION"
+if [[ "${target_platform}" == "linux-64" || "${target_platform}" == "linux-aarch64" ]]; then
+    # https://github.com/conda-forge/jaxlib-feedstock/issues/310
+    # Explicitly force non-executable stack to fix compatibility with glibc 2.41, due to:
+    # xla_extension.so: cannot enable executable stack as shared object requires: Invalid argument
+    LDFLAGS+=" -Wl,-z,noexecstack"
 fi
+export CFLAGS="${CFLAGS} -DNDEBUG -Dabsl_nullable= -Dabsl_nonnull="
+export CXXFLAGS="${CXXFLAGS} -DNDEBUG -Dabsl_nullable= -Dabsl_nonnull="
 
-if [[ "${target_platform}" == linux-* ]]; then
-    export BUILD_FLAGS="${BUILD_FLAGS} --nouse_clang"
+if [[ "${cuda_compiler_version:-None}" != "None" ]]; then
+    if [[ ${cuda_compiler_version} == 12* ]]; then
+        export HERMETIC_CUDA_COMPUTE_CAPABILITIES=sm_60,sm_70,sm_75,sm_80,sm_86,sm_89,sm_90,sm_100,sm_120,compute_120
+    else
+        export HERMETIC_CUDA_COMPUTE_CAPABILITIES=sm_75,sm_80,sm_86,sm_89,sm_90,sm_100,sm_110,sm_120,compute_120
+    fi
+    if [[ "${target_platform}" == "linux-64" ]]; then
+        export CUDA_ARCH="x86_64-linux"
+    elif [[ "${target_platform}" == "linux-aarch64" ]]; then
+	export CUDA_ARCH="sbsa-linux"
+    else
+	echo "Unknown architecture for CUDA"
+	exit 1
+    fi
+    export CUDA_HOME="${BUILD_PREFIX}/targets/${CUDA_ARCH}"
+    export TF_CUDA_PATHS="${BUILD_PREFIX}/targets/${CUDA_ARCH},${PREFIX}/targets/${CUDA_ARCH}"
+    # Needed for some nvcc binaries
+    export PATH=$PATH:${BUILD_PREFIX}/nvvm/bin
+    # XLA can only cope with a single cuda header include directory, merge both
+    rsync -a ${PREFIX}/targets/${CUDA_ARCH}/include/ ${BUILD_PREFIX}/targets/${CUDA_ARCH}/include/
+
+    # Although XLA supports a non-hermetic build, it still tries to find headers in the hermetic locations.
+    # We do this in the BUILD_PREFIX to not have any impact on the resulting jaxlib package.
+    # Otherwise, these copied files would be included in the package.
+    rm -rf ${BUILD_PREFIX}/targets/${CUDA_ARCH}/include/third_party
+    mkdir -p ${BUILD_PREFIX}/targets/${CUDA_ARCH}/include/third_party/gpus/cuda/extras/CUPTI
+    cp -r ${PREFIX}/targets/${CUDA_ARCH}/include ${BUILD_PREFIX}/targets/${CUDA_ARCH}/include/third_party/gpus/cuda/
+    cp -r ${PREFIX}/targets/${CUDA_ARCH}/include ${BUILD_PREFIX}/targets/${CUDA_ARCH}/include/third_party/gpus/cuda/extras/CUPTI/
+    mkdir -p ${BUILD_PREFIX}/targets/${CUDA_ARCH}/include/third_party/gpus/cudnn
+    cp ${PREFIX}/include/cudnn*.h ${BUILD_PREFIX}/targets/${CUDA_ARCH}/include/third_party/gpus/cudnn/
+    mkdir -p ${BUILD_PREFIX}/targets/${CUDA_ARCH}/include/third_party/nccl
+    cp ${PREFIX}/include/nccl*.h ${BUILD_PREFIX}/targets/${CUDA_ARCH}/include/third_party/nccl/
+    export LOCAL_CUDA_PATH="${BUILD_PREFIX}/targets/${CUDA_ARCH}"
+    export LOCAL_CUDNN_PATH="${PREFIX}/targets/${CUDA_ARCH}"
+    export LOCAL_NCCL_PATH="${PREFIX}/targets/${CUDA_ARCH}"
+    mkdir -p ${BUILD_PREFIX}/targets/${CUDA_ARCH}/bin
+    test -f ${BUILD_PREFIX}/targets/${CUDA_ARCH}/bin/ptxas || ln -s $(which ptxas) ${BUILD_PREFIX}/targets/${CUDA_ARCH}/bin/ptxas
+    test -f ${BUILD_PREFIX}/targets/${CUDA_ARCH}/bin/nvlink || ln -s $(which nvlink) ${BUILD_PREFIX}/targets/${CUDA_ARCH}/bin/nvlink
+    test -f ${BUILD_PREFIX}/targets/${CUDA_ARCH}/bin/fatbinary || ln -s $(which fatbinary) ${BUILD_PREFIX}/targets/${CUDA_ARCH}/bin/fatbinary
+
+    export TF_CUDA_VERSION="${cuda_compiler_version}"
+    export TF_CUDNN_VERSION="${cudnn}"
+    if [[ "${target_platform}" == "linux-aarch64" ]]; then
+        export TF_CUDA_PATHS="${CUDA_HOME}/targets/sbsa-linux,${TF_CUDA_PATHS}"
+    fi
+    export TF_NEED_CUDA=1
+    export TF_NCCL_VERSION=$(pkg-config nccl --modversion | grep -Po '\d+\.\d+')
+    export CUDA_COMPILER_MAJOR_VERSION=$(echo "$cuda_compiler_version" | cut -d '.' -f 1)
+    CUDA_ARGS="--wheels=jaxlib,jax-cuda-plugin,jax-cuda-pjrt \
+               --cuda_compute_capabilities=$HERMETIC_CUDA_COMPUTE_CAPABILITIES \
+               --cuda_major_version=${CUDA_COMPILER_MAJOR_VERSION} \
+               --cuda_version=$TF_CUDA_VERSION \
+               --cudnn_version=$TF_CUDNN_VERSION \
+               --build_cuda_with_clang"
 fi
 
 source gen-bazel-toolchain
 
-cat >> .bazelrc <<EOF
+cat >> .bazelrc.user <<EOF
 
 build --crosstool_top=//bazel_toolchain:toolchain
+build --platforms=//bazel_toolchain:target_platform
+build --host_platform=//bazel_toolchain:build_platform
+build --extra_toolchains=//bazel_toolchain:cc_cf_toolchain
+build --extra_toolchains=//bazel_toolchain:cc_cf_host_toolchain
 build --logging=6
 build --verbose_failures
 build --toolchain_resolution_debug
 build --define=PREFIX=${PREFIX}
 build --define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include
-build --local_cpu_resources=${CPU_COUNT}"
+build --define=with_cross_compiler_support=true
+build --repo_env=GRPC_BAZEL_DIR=${PREFIX}/share/bazel/grpc/bazel
+
+# We need to define a dummy value for this as we delete everything else for build_cuda_with_nvcc
+build:build_cuda_with_nvcc --action_env=CONDA_USE_NVCC=1
 EOF
 
-# Unvendor from XLA using TF_SYSTEM_LIBS. You can find the list of supported libraries at:  
-# https://github.com/openxla/xla/blob/main/third_party/tsl/third_party/systemlibs/syslibs_configure.bzl#L11
-# TODO: RE2 fails with: external/xla/xla/hlo/parser/hlo_lexer.cc:244:8: error: no matching function for call to 'Consume'
-  # if (!RE2::Consume(&consumable, *payload_pattern)) 
-# Removed com_googlesource_code_re2
-# Removed com_google_protobuf: Upstream discourages dynamically linking with protobuf https://github.com/conda-forge/jaxlib-feedstock/issues/89
+# Use a fixed number instead of CPU_COUNT on linux-aarch64
+if [[ "${target_platform}" == "linux-aarch64" ]]; then
+  echo "build --local_resources=cpu=8" >> .bazelrc.user
+else
+  echo "build --local_resources=cpu=${CPU_COUNT}" >> .bazelrc.user
+fi
+
+if [[ "${target_platform}" == "osx-arm64" || "${target_platform}" != "${build_platform}" ]]; then
+  echo "build --cpu=${TARGET_CPU}" >> .bazelrc.user
+fi
+
+# For debugging
+# CUSTOM_BAZEL_OPTIONS="${CUSTOM_BAZEL_OPTIONS} --bazel_options=--subcommands"
+
+# Force static linkage with protobuf to avoid definition collisions,
+# see https://github.com/conda-forge/jaxlib-feedstock/issues/89
+#
+# Thus: don't add com_google_protobuf here.
 export TF_SYSTEM_LIBS="
-  absl_py,
-  astor_archive,
-  astunparse_archive,
   boringssl,
   com_github_googlecloudplatform_google_cloud_cpp,
   com_github_grpc_grpc,
   com_google_absl,
-  curl,
-  cython,
-  dill_archive,
-  double_conversion,
   flatbuffers,
-  functools32_archive,
-  gast_archive,
-  gif,
-  hwloc,
-  icu,
-  jsoncpp_git,
-  libjpeg_turbo,
-  nasm,
-  nsync,
-  org_sqlite,
-  pasta,
-  png,
-  pybind11,
-  six_archive,
-  snappy,
-  tblib_archive,
-  termcolor_archive,
-  typing_extensions_archive,
-  wrapt,
-  zlib"
+  zlib
+"
 
-bazel clean --expunge
+# # Anaconda XLA unvendoring
+# export TF_SYSTEM_LIBS="
+#   ${TF_SYSTEM_LIBS},
+#   absl_py,
+#   astor_archive,
+#   astunparse_archive,
+#   curl,
+#   cython,
+#   dill_archive,
+#   double_conversion,
+#   functools32_archive,
+#   gast_archive,
+#   gif,
+#   hwloc,
+#   icu,
+#   jsoncpp_git,
+#   libjpeg_turbo,
+#   nasm,
+#   nsync,
+#   org_sqlite,
+#   pasta,
+#   png,
+#   pybind11,
+#   six_archive,
+#   snappy,
+#   tblib_archive,
+#   termcolor_archive,
+#   typing_extensions_archive,
+#   wrapt
+# "
 
-echo "Building...."
-${PYTHON} build/build.py ${BUILD_FLAGS}
-echo "Building done."
+if [[ "${target_platform}" == "osx-64" ]]; then
+    export TF_SYSTEM_LIBS="${TF_SYSTEM_LIBS},onednn"
+fi
+
+# Mark as a release build
+EXTRA="--bazel_options=--repo_env=ML_WHEEL_TYPE=release ${CUDA_ARGS:-}"
+
+if [[ "${target_platform}" == "osx-arm64" || "${target_platform}" != "${build_platform}" ]]; then
+    EXTRA="${EXTRA} --target_cpu ${TARGET_CPU}"
+fi
+
+# Never use the Apple toolchain
+sed -i '/local_config_apple/d' .bazelrc
+if [[ "${target_platform}" == linux-* ]]; then
+    EXTRA="${EXTRA} --clang_path $CC"
+
+    # Remove incompatible argument from bazelrc
+    sed -i '/Qunused-arguments/d' .bazelrc
+    # Don't override our toolchain for CUDA
+    sed -i '/TF_NVCC_CLANG/{N;d}' .bazelrc
+    # Keep using our toolchain
+    sed -i '/--crosstool_top=@local_config_cuda/d' .bazelrc
+fi
+
+${PYTHON} build/build.py build \
+    --target_cpu_features default \
+    --python_version $PY_VER \
+    ${EXTRA}
 
 # Clean up to speedup postprocessing
-echo "Issuing bazel clean..."
 pushd build
 bazel clean --expunge
 popd
 
-echo "Issuing bazel shutdown..."
-bazel shutdown
+pushd $SP_DIR
+$PYTHON -m pip install $SRC_DIR/dist/jaxlib-*.whl --no-build-isolation --no-deps -vv
 
-echo "Installing jaxlib wheel..."
-${PYTHON} -m pip install dist/jaxlib-*.whl --no-build-isolation --no-deps
+# Add INSTALLER file and remove RECORD, workaround for
+# https://github.com/conda-forge/jaxlib-feedstock/issues/293
+JAXLIB_DIST_INFO_DIR="${SP_DIR}/jaxlib-${PKG_VERSION}.dist-info"
+echo "conda" > "${JAXLIB_DIST_INFO_DIR}/INSTALLER"
+rm -f "${JAXLIB_DIST_INFO_DIR}/RECORD"
+
+if [[ "${cuda_compiler_version:-None}" != "None" ]]; then
+  $PYTHON -m pip install $SRC_DIR/dist/jax_cuda*_plugin*.whl --no-build-isolation --no-deps -vv
+  $PYTHON -m pip install $SRC_DIR/dist/jax_cuda*_pjrt*.whl --no-build-isolation --no-deps -vv
+
+  # Add INSTALLER file and remove RECORD, workaround for
+  # https://github.com/conda-forge/jaxlib-feedstock/issues/293
+  JAX_CUDA_PJRT_DIST_INFO_DIR="${SP_DIR}/jax_cuda${CUDA_COMPILER_MAJOR_VERSION}_pjrt-${PKG_VERSION}.dist-info"
+  echo "conda" > "${JAX_CUDA_PJRT_DIST_INFO_DIR}/INSTALLER"
+  rm -f "${JAX_CUDA_PJRT_DIST_INFO_DIR}/RECORD"
+  JAX_CUDA_PLUGIN_DIST_INFO_DIR="${SP_DIR}/jax_cuda${CUDA_COMPILER_MAJOR_VERSION}_plugin-${PKG_VERSION}.dist-info"
+  echo "conda" > "${JAX_CUDA_PLUGIN_DIST_INFO_DIR}/INSTALLER"
+  rm -f "${JAX_CUDA_PLUGIN_DIST_INFO_DIR}/RECORD"
+
+  # Regression test for https://github.com/conda-forge/jaxlib-feedstock/issues/320
+  if [[ "${target_platform}" == linux-* ]]; then
+    # Scan all .so files in both plugin directories and error if any FLAGS_* symbols are present.
+    declare -a PLUGIN_DIRS=(
+      "${SP_DIR}/jax_plugins/xla_cuda${CUDA_COMPILER_MAJOR_VERSION}"
+      "${SP_DIR}/jax_cuda${CUDA_COMPILER_MAJOR_VERSION}_plugin"
+    )
+    echo "Scanning CUDA plugin directories for .so files and FLAGS_* symbols:"
+    for DIR in "${PLUGIN_DIRS[@]}"; do
+      if [[ -d "${DIR}" ]]; then
+        echo " - ${DIR}"
+        mapfile -t SO_FILES < <(find "${DIR}" -type f -name '*.so' -print | sort)
+        if (( ${#SO_FILES[@]} == 0 )); then
+          echo "   (no .so files found)"
+          continue
+        fi
+        echo "   .so files:"
+        for SO in "${SO_FILES[@]}"; do
+          echo "     * ${SO}"
+        done
+        # Prefer nm -s as requested; fall back to plain nm if -s is unsupported to avoid hard failure.
+        # Fail the build if any symbol starting with FLAGS_ is present.
+        for SO in "${SO_FILES[@]}"; do
+          SYMBOLS_OUTPUT=$(nm -s "${SO}" 2>/dev/null || nm "${SO}")
+          if echo "${SYMBOLS_OUTPUT}" | grep -E '(^|[^A-Za-z0-9_])FLAGS_[A-Za-z0-9_]+' >/dev/null; then
+            echo "Error: Unexpected FLAGS_* symbols found in ${SO}:" >&2
+            echo "----------------------------------------" >&2
+            echo "${SYMBOLS_OUTPUT}" | grep -E '(^|[^A-Za-z0-9_])FLAGS_[A-Za-z0-9_]+' >&2 || true
+            echo "----------------------------------------" >&2
+            exit 1
+          fi
+        done
+      else
+        echo "Warning: ${DIR} not found; skipping" >&2
+      fi
+    done
+    echo "No FLAGS_* symbols found in the CUDA plugin directory, the test was successul"
+  fi
+fi
+
+popd
